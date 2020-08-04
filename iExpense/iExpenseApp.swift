7b801a9ca2cbc2f8b5203546fcb60e70db5ef118
Alex McPherson <alex.mcpherson@worldpay.com>
2020-08-04 17:01:38 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alex McPherson on 04/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

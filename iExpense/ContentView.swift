6cd6d6e7f117654aff74c208bc4e62e472d59742
Alex McPherson <alex.mcpherson@worldpay.com>
2020-08-06 13:08:29 +0100

    Dismissing modal view

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,38 +7,67 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
-    let id = UUID()
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Int
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let data = try? encoder.encode(items) {
+                UserDefaults.standard.set(data, forKey: "Items")
+            }
+        }
+    }
+
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let data = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = data
+                return
+            }
+        }
+        self.items = []
+    }
 }
 
 struct ContentView: View {
 
     @ObservedObject var expenses = Expenses()
+    @State private var showingAddExpense = false
 
     var body: some View {
         NavigationView {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text("$\(item.amount)")
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationBarTitle("iExpense")
             .navigationBarItems(trailing:
                 Button(action: {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    self.expenses.items.append(expense)
+                    self.showingAddExpense = true
                 }) {
                     Image(systemName: "plus")
                 }
             )
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
         }
     }
 
